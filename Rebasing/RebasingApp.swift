b291f0958448f1a9ca24270d3aae332472dfdacc
prashanthPadmashali <prashanthkumarshettigar@gmail.com>
2025-03-31 14:54:37 +0530

    Initial Commit

---
+++ Rebasing/RebasingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RebasingApp.swift
+//  Rebasing
+//
+//  Created by mac on 31/03/25.
+//
+
+import SwiftUI
+
+@main
+struct RebasingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
