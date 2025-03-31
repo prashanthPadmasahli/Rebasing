87a700358c96586864e2dba2ffd7c6fce69a514e
prashanthPadmashali <prashanthkumarshettigar@gmail.com>
2025-03-31 14:59:21 +0530

    Home and Detail view added

---
+++ Rebasing/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Rebasing
+//
+//  Created by mac on 31/03/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Deatil")
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ Rebasing/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Rebasing
+//
+//  Created by mac on 31/03/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("HomeView")
+    }
+}
+
+#Preview {
+    HomeView()
+}
